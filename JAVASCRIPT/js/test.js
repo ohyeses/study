7d5f4a472f2e4b70b21d14b42298be6fe9d97c06
ohyeses <ohyeses@naver.com>
2021-03-04 20:38:29 +0900

    0304

---
+++ JAVASCRIPT/js/test.js
@@ -0,0 +1,9 @@
+//  자바스크립트를 쓰려면 <script>태그를 써야한다.
+
+    // 자바 언어를 작성하는 공간.
+    function ch() {
+        //ch라는 이름의 함수를 만든다.
+        document.querySelector("body").style.backgroundColor = "yellow"; /* 도큐먼트 -> 객체 html의 모든 코드를 담고 있는 것 */
+                            /* querySelector-> 선택자좀 가져다줘. */
+                            /* body의 스타일을 바꿔주세요, 배경색상을 노란색으로 */
+    }
